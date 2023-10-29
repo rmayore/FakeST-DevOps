ce66d618bb36944b5f657319aa92784359486fbf
Mayore <rmayore@livinggoods.org>
2023-10-29 18:00:55 +0300

    FST-17: Added subnet module
    Signed-off-by: Mayore <rmayore@livinggoods.org>

---
+++ tf-modules/subnet/output.tf
@@ -0,0 +1,6 @@
+output "private_subnet_ids" {
+  value = module.subnet.private_subnet_ids
+}
+output "public_subnet_arns" {
+  value = module.subnet.public_subnet_arns
+}
--- tf-modules/vpc/variables.tf
+++ tf-modules/vpc/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS Region"
-}
 variable "app_name" {
   type        = string
   description = "App Name"
