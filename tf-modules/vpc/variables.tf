0240c746c952c97e01ab7d8c21978016e07fbfb7
Mayore <rmayore@livinggoods.org>
2023-10-29 16:31:38 +0300

    FST-17: Added vpc module
    Signed-off-by: Mayore <rmayore@livinggoods.org>

---
+++ tf-modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+}
+variable "app_name" {
+  type        = string
+  description = "App Name"
+}
+variable "namespace" {
+  type        = string
+  description = "Namespace"
+}
+variable "stage" {
+  type        = string
+  description = "Stage"
+}
