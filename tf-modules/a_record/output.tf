679bd6000dae527727438c672addeaf73c9c2ec5
Mayore <rmayore@livinggoods.org>
2023-11-13 17:55:27 +0300

    FST-17: Added zone name module
    Signed-off-by: Mayore <rmayore@livinggoods.org>

---
+++ tf-modules/a_record/output.tf
@@ -0,0 +1,6 @@
+output "name" {
+  value = aws_route53_record.a_record.name
+}
+output "fqdn" {
+  value = aws_route53_record.a_record.fqdn
+}
---
+++ tf-modules/a_record/variables.tf
@@ -0,0 +1,12 @@
+variable "zone_id" {
+  type        = string
+  description = "Zone Id"
+}
+variable "zone_name" {
+  type        = string
+  description = "Zone Name"
+}
+variable "eip" {
+  type        = string
+  description = "Elastic IP"
+}
